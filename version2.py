3725021eb21dfb008055788fcbff66fbb0f78953
Austin Herring <awh44@drexel.edu>
2015-03-31 21:20:49 -0400

    Added all of the logic for gathering the data.

--- version2.py
+++ version2.py
@@ -5,6 +5,27 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
+def check_all_terms(driver):
+	classes = {}
+	checked = []
+	while True:
+		dropdown = driver.find_element_by_name("p_term")
+		options = dropdown.find_elements_by_tag_name("option")
+		for opt in options:
+			if opt.text not in checked and opt.text != "None" and str(opt.text).find("Semester") == -1:
+				checked.append(opt.text)
+				quarter = str(opt.text)
+				if quarter.endswith(" (View only)"):
+					quarter = quarter[:-len(" (View only)")]
+				opt.click()
+				driver.find_element_by_xpath("//*[@value='Submit']").click()
+				for handle in driver.window_handles:
+					driver.switch_to_window(handle)
+				check_all_subjects(driver, classes, quarter)
+				print classes
+				driver.back()
+				break
+
 def check_all_subjects(driver, classes, quarter):
 	checked = []
 	while True:
@@ -18,15 +39,19 @@ def check_all_subjects(driver, classes, quarter):
 				numbers = driver.find_elements_by_class_name("dddefault")
 				for num in numbers:
 					if re.match("[0-9]{3}", str(num.text)) != None:
-						#classes =  
-						print num.text
+						deptPlusNum = textval + " " + num.text
+						if deptPlusNum in classes:
+							classes[deptPlusNum] += [quarter]
+						else:
+							classes[deptPlusNum] = [quarter]
 				driver.back()
 				break
 			elif checked[len(checked) - 1] == textval:
 				ActionChains(driver).key_down(Keys.CONTROL).perform()
 				sub.click()
 				ActionChains(driver).key_up(Keys.CONTROL).perform()
-			if textval == "Writing":
+			
+			if textval == "Animation":#Writing":
 				return
 
 if len(sys.argv) < 3:
@@ -47,19 +72,4 @@ driver.close()
 for handle in driver.window_handles:
 	driver.switch_to_window(handle)
 
-dropdown = driver.find_element_by_name("p_term")
-options = dropdown.find_elements_by_tag_name("option")
-quarter = ""
-for opt in options:
-	if opt.text != "None" and str(opt.text).find("Semester") == -1:
-		quarter = str(opt.text)
-		opt.click()
-		#TODO - remove
-		break
-
-driver.find_element_by_xpath("//*[@value='Submit']").click()
-
-for handle in driver.window_handles:
-	driver.switch_to_window(handle)
-
-check_all_subjects(driver)
+check_all_terms(driver)
