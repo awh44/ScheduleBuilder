aaf021053ae3a490df3db77f35f79115aa50b29d
Austin Herring <awh44@drexel.edu>
2015-04-04 19:24:23 -0400

    Changed setup/version2.py to insert directly into database.
    This change should also theoretically allow this script to be reused whenever new terms are added to Drexel One. Note also that right now the script is set to stop after grabbing the data for only one quarter (Spring 2014-2015)

--- setup/version2.py
+++ setup/version2.py
@@ -1,50 +1,101 @@
 #!/usr/bin/python
-import sys
+
 import re
+import sqlite3
+import sys
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-def check_all_terms(driver):
-	classes = {}
+STOP_QUARTER = "Spring Quarter 14-15"#"Summer Quarter 98-99"
+
+def sanitize_quarter(quarter):
+	return quarter[:-len(" (View only)")] if quarter.endswith(" (View only)") else quarter
+
+def get_course_obj(c, abbrev, number):
+	return c.execute("SELECT course_id FROM courses WHERE subj_id = ? AND number = ?", (abbrev, number)).fetchone()
+ 
+
+def check_all_terms(driver, conn):
+	global STOP_QUARTER
+
+	#Determine all of the quarters which have already been added to the database
 	checked = []
+	c = conn.cursor()
+	quartersobj = c.execute("SELECT quarter FROM quarters")
+	for quarterobj in quartersobj:
+		checked.append(str(quarterobj[0]))
+
 	while True:
 		dropdown = driver.find_element_by_name("p_term")
 		options = dropdown.find_elements_by_tag_name("option")
 		for opt in options:
-			quarter = str(opt.text)
+			quarter = sanitize_quarter(str(opt.text))
 			if quarter not in checked and quarter != "None" and quarter.find("Semester") == -1:
+				c.execute("INSERT INTO quarters(quarter) VALUES(?)", (quarter,))
 				checked.append(quarter)
-				quarter = quarter
-				if quarter.endswith(" (View only)"):
-					quarter = quarter[:-len(" (View only)")]
+				
 				opt.click()
 				driver.find_element_by_xpath("//*[@value='Submit']").click()
-				check_all_subjects(driver, classes, quarter)
+				check_all_subjects(driver, quarter, c)
+				
+				#make sure that all of the subjects worked first, then commit them all at once
+				conn.commit()
+
+				#then navigate back and do again
 				driver.back()
 				break
 			
-			if quarter == "Summer Quarter 98-99 (View only)":
-				return classes
+			if quarter == STOP_QUARTER:
+				return
 
-def check_all_subjects(driver, classes, quarter):
+def check_all_subjects(driver, quarter, c):
+	quarter_id = c.execute("SELECT quarter_id FROM quarters WHERE quarter = ?", (quarter,)).fetchone()[0]
 	checked = []
 	while True:
 		subjects = driver.find_elements_by_tag_name("option")
 		for sub in subjects:
+			#Get the name of the subject
 			textval = sub.text
+	
 			if textval not in checked:
+				#Note that this subject has now been checked
 				checked.append(textval)
+
+				#Select the subject from the database
+				subobj = c.execute("SELECT subj_id FROM subjects WHERE name = ?", (textval,)).fetchone()
+				if subobj == None:
+					#If the subject could not be found in the database, make a note of it - the
+					#the database value will have to be manually changed. Then go to the next sub
+					print "Could not find subject: " + textval
+					continue
+
+				#However, if it could be found, and the subject has not been checked, continue
+				#by noting that it's been checked, selecting in the multiselect, and clicking
 				sub.click()
 				driver.find_element_by_xpath("//*[@value='Course Search']").click()
+
+				#Go back to the subobj and pull out the subj_id, which is actually the subject
+				#abbreviation, e.g., "CS" or "MATH"
+				subj_id = subobj[0]
+
+				#Pull out all of the course numbers
 				numbers = driver.find_elements_by_class_name("dddefault")
 				for num in numbers:
 					if re.match("[0-9]{3}", str(num.text)) != None:
-						deptPlusNum = textval + " " + num.text
-						if deptPlusNum in classes:
-							classes[deptPlusNum] += [quarter]
-						else:
-							classes[deptPlusNum] = [quarter]
+						courseobj = get_course_obj(c, subj_id, num.text)
+						if courseobj == None:
+							c.execute("INSERT INTO courses(subj_id, number) VALUES(?, ?)", (subj_id, num.text))
+							courseobj = get_course_obj(c, subj_id, num.text)
+
+						course_id = courseobj[0]
+						c.execute("INSERT INTO quarters_for_courses(quarter_id, course_id) VALUES(?, ?)", (quarter_id, course_id))
+						#deptPlusNum = textval + " " + num.text
+						#if deptPlusNum in classes:
+						#	classes[deptPlusNum] += [quarter]
+						#else:
+						#	classes[deptPlusNum] = [quarter]
 				driver.back()
 				break
 			elif checked[len(checked) - 1] == textval:
@@ -73,11 +124,6 @@ driver.close()
 for handle in driver.window_handles:
 	driver.switch_to_window(handle)
 
-classes = check_all_terms(driver)
-fout = open("coursedata", "w")
-for course in sorted(classes.keys()):
-	fout.write(course + " | ")
-	for quarter in classes[course]:
-		fout.write(quarter + " | ")
-	fout.write("\n")
-fout.close()
+conn = sqlite3.connect("../data/courses.db")
+check_all_terms(driver, conn)
+conn.close()
