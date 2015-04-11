0f146f51ee1b03e6f9cebe0a66e5bc5acf2952f4
Austin Herring <awh44@drexel.edu>
2015-04-11 10:26:39 -0400

    Added comments. Changed exit conditions for maintainability
    Also added an extra condition when checking the quarters for 'semester' to also look for 'Contiuning,' which doesn't need to be checked either.

--- setup/get_quarters_for_courses.py
+++ setup/get_quarters_for_courses.py
@@ -32,7 +32,7 @@ def check_all_terms(driver, conn):
 		options = dropdown.find_elements_by_tag_name("option")
 		for opt in options:
 			quarter = sanitize_quarter(str(opt.text))
-			if quarter not in checked and quarter != "None" and quarter.find("Semester") == -1:
+			if quarter not in checked and quarter != "None" and quarter.find("Semester") == -1 and quarter.find("Continuing") == -1:
 				c.execute("INSERT INTO quarters(quarter) VALUES(?)", (quarter,))
 				checked.append(quarter)
 				
@@ -52,54 +52,56 @@ def check_all_terms(driver, conn):
 
 def check_all_subjects(driver, quarter, c):
 	quarter_id = c.execute("SELECT quarter_id FROM quarters WHERE quarter = ?", (quarter,)).fetchone()[0]
+	last_sub_text = driver.find_element_by_xpath("//*[local-name()='option'][last()]").text
 	checked = []
 	while True:
 		subjects = driver.find_elements_by_tag_name("option")
 		for sub in subjects:
 			#Get the name of the subject
-			subj_id = str(sub.get_attribute("value"))
+			if sub.get_attribute("value") != None:
+				subj_id = str(sub.get_attribute("value"))
 	
-			if subj_id not in checked:
-				#Note that this subject has now been checked
-				checked.append(subj_id)
+				if subj_id not in checked and sub.text != "":
+					#Note that this subject has now been checked
+					checked.append(subj_id)
 
-				#Make sure the subject is in the database, so first select it
-				subobj = c.execute("SELECT * FROM subjects WHERE subj_id = ?", (subj_id,)).fetchone()
-				if subobj == None:
-					print "Couldn't find subject", sub.text, ", inserting now."
-					#If the subject could not be found in the database, make sure to insert it
-					c.execute("INSERT INTO subjects(subj_id, name) VALUES(?, ?)", (subj_id, sub.text))
+					#Make sure the subject is in the database, so first select it
+					subobj = c.execute("SELECT * FROM subjects WHERE subj_id = ?", (subj_id,)).fetchone()
+					if subobj == None:
+						print "Couldn't find subject", sub.text, ", inserting now."
+						#If the subject could not be found in the database, make sure to insert it
+						c.execute("INSERT INTO subjects(subj_id, name) VALUES(?, ?)", (subj_id, sub.text))
 
-				#However, if it could be found, and the subject has not been checked, continue
-				#by noting that it's been checked, selecting in the multiselect, and clicking
-				sub.click()
-				driver.find_element_by_xpath("//*[@value='Course Search']").click()
+					#However, if it could be found, continue selecting in the multiselect and clicking
+					sub.click()
+					driver.find_element_by_xpath("//*[@value='Course Search']").click()
 
-				#Pull out all of the course numbers
-				numbers = driver.find_elements_by_class_name("dddefault")
-				for num in numbers:
-					if re.match("[0-9]{3}", str(num.text)) != None:
-						courseobj = get_course_obj(c, subj_id, num.text)
-						#If the course hasn't been seen before, make sure to insert it
-						if courseobj == None:
-							course_name = num.find_element_by_xpath(".//following-sibling::*[local-name()='td']").text
-							c.execute("INSERT INTO courses(subj_id, number, name) VALUES(?, ?, ?)", (subj_id, num.text, course_name))
+					#Pull out all of the course numbers - however, this class also includes other elements on the page
+					numbers = driver.find_elements_by_class_name("dddefault")
+					for num in numbers:
+						#Because the CSS class also includes other elements on the page, make sure the inner text is a course number
+						if re.match("[0-9]{3}", str(num.text)) != None:
 							courseobj = get_course_obj(c, subj_id, num.text)
+							#If the course hasn't been seen before, make sure to insert it
+							if courseobj == None:
+								course_name = num.find_element_by_xpath(".//following-sibling::*[local-name()='td']").text
+								c.execute("INSERT INTO courses(subj_id, number, name) VALUES(?, ?, ?)", (subj_id, num.text, course_name))
+								courseobj = get_course_obj(c, subj_id, num.text)
 
-						course_id = courseobj[0]
-						c.execute("INSERT INTO quarters_for_courses(quarter_id, course_id) VALUES(?, ?)", (quarter_id, course_id))
-				
-				#Go back for the next subject
-				driver.back()
-				break
-			elif checked[len(checked) - 1] == subj_id:
-				#If looking at the previously seen subject, hold control and click it to deselect it
-				ActionChains(driver).key_down(Keys.CONTROL).perform()
-				sub.click()
-				ActionChains(driver).key_up(Keys.CONTROL).perform()
+							course_id = courseobj[0]
+							c.execute("INSERT INTO quarters_for_courses(quarter_id, course_id) VALUES(?, ?)", (quarter_id, course_id))
+					
+					#Go back for the next subject
+					driver.back()
+					break
+				elif checked[len(checked) - 1] == subj_id:
+					#If looking at the previously seen subject, hold control and click it to deselect it
+					ActionChains(driver).key_down(Keys.CONTROL).perform()
+					sub.click()
+					ActionChains(driver).key_up(Keys.CONTROL).perform()
 			
-			if subj_id == "WRIT":
-				return
+				if sub.text == last_sub_text:
+					return
 
 if len(sys.argv) < 3:
 	print "Please include your username and password for one.drexel.edu."
