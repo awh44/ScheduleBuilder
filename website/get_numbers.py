d4b01a60289a97688c7800cc65305fe9214dcc86
Austin Herring <awh44@drexel.edu>
2015-04-25 11:30:36 -0400

    Added a passthru to Python because tux supports neither SQLite in PHP nor cgi-bin

--- website/get_numbers.py
+++ website/get_numbers.py
@@ -3,12 +3,7 @@
 import sqlite3
 import sys
 
-print "hi", sys.argv[1]
 conn = sqlite3.connect("../data/courses.db")
-print "ho"
 c = conn.cursor()
-print "hum"
 results = c.execute("SELECT * FROM courses WHERE subj_id = ?", (sys.argv[1],))
-print "fiddle"
 print results.fetchone()
-print "dee"
