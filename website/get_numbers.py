0d76476142640c5662f2a2ed1ef9e79163e83de3
Austin Herring <awh44@drexel.edu>
2015-04-25 11:01:44 -0400

    Started a Python equivalent of get_numbers.php

---
+++ website/get_numbers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import sqlite3
+import sys
+
+print "hi"
+conn = sqlite3.connect("../../data/courses.db")
+print "ho"
+c = conn.cursor()
+print "hum"
+results = c.execute("SELECT * FROM courses WHERE subj_id = ?", (sys.argv[1],))
+print "fiddle"
+print results.fetchone()
+print "dee"
