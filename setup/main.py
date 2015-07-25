92f5e0e1f39351cddafb3baa9897d9ec0b5fa116
Austin Herring <awh44@drexel.edu>
2015-07-24 22:05:18 -0400

    Made main program cleaner.

--- setup/main.py
+++ setup/main.py
@@ -7,18 +7,30 @@ from SiteParser import SiteParser
 from TMSParser import TMSParser
 from DrexelOneParser import DrexelOneParser
 
+def is_one_parser(parser_type):
+	return parser_type == "one"
+
+def is_tms_parser(parser_type):
+	return parser_type == "tms"
+
+def create_one_parser(username, password):
+	return DrexelOneParser("one_courses_500.db", argv[2], argv[3])
+
+def create_tms_parser()
+	return TMSParser("courses_500.db")
+
 def main(argv):
 	if len(argv) < 2:
 		print "Supply which site should be parsed, either 'one' or 'tms', as a command line argument."
 		return 1
 
-	if argv[1] == "one":
+	if is_one_parser(argv[1])
 		if len(argv) < 4:
 			print "Please supply a username and password as command line arguments."
 			return 1
-		parser = DrexelOneParser("one_courses_500.db", argv[2], argv[3])
-	elif argv[1] == "tms":
-		parser = TMSParser("courses_500.db")
+		parser = create_one_parser(argv[2], argv[3])
+	elif is_tms_parser(tms)
+		parser = create_tms_parser()
 
 	else:
 		print "Parser type not recognized. Please use either 'one' or 'tms' as the first command line argument."
@@ -30,7 +42,7 @@ def main(argv):
 		except Exception, err:
 			traceback.print_exc()
 			parser.cleanup()
-			parser = DrexelOneParser("one_courses_500.db") if argv[1] == "one" else TMSParser("courses_500.db")
+			parser = create_one_parser(argv[2], argv[3]) if is_one_parser(argv[1]) else create_tms_parser()
 
 if __name__ == "__main__":
 	sys.exit(main(sys.argv))
